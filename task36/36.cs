5d0b6316578020357e5c312a421d51459c162869
NoAnnY <noanny@mail.ru>
2022-11-27 21:11:35 +0300

    36Done right"

--- task36/36.cs
+++ task36/36.cs
@@ -1,8 +1,6 @@
 ﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
-// Не определилась с уверенностью на 100%, что следует рассматривать под позицией элемента: номер индекса или "общепринятый порядковы
-//номер, поэтому у меня 2 варианта в решении 
 
 Console.WriteLine("Задайте длину массива");
 int userlenght = int.Parse(Console.ReadLine()!); 
@@ -10,8 +8,8 @@ int[] massive = GetArray(userlenght);
 Console.WriteLine("Полученный массив:");
 Console.Write("["+ string.Join(", ", massive)+ "]");//печатаем массив
 Console.WriteLine();
-Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях, если исходить из числа индекса, равна: {SumEvenI(massive)}");
-Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях, если исходить из порядкового номера элемента, равна: {SumEvenCount(massive)}");
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях равна: {SumEvenI(massive)}");
+
 //Пишем функцию, которая составит нам массив, соответствующий заданным условиям
 int[] GetArray (int lenght, int minValue = -100, int maxValue = 100)
 {
@@ -32,16 +30,6 @@ int SumEvenI(int[] array)
     for (int i = 0; i <array.Length; i++)
     {
         if (i !=0 & i % 2 != 0) count += array[i];
-    }
-    return count;
-}
-//Функция для вычисления суммы элементов массива, стоящих на нечетных позициях (индекс которых является четным числом или нулем)
-int SumEvenCount(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i <array.Length; i++)
-    {
-        if (i % 2 == 0) count += array[i];
     }
     return count;
 }
